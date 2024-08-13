fd1ac3cd7c7795e096ddb7269b94afdc110b34a1
AsmitBro <asmitkhanal335@gmail.com>
2024-08-13 08:55:09 +0545

    certificate added

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -6,6 +6,6 @@ export default function page() {
       <section className="flex justify-center items-center bg-slate-500 h-full">
         Wait It will be Developed ASAP.
       </section>
-    </>
+     </>
   );
 }
